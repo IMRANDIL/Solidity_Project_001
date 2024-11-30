a58cefb1332e73116f22b458c19e1a053a464add
IMRANDIL <aliimranadil2@gmail.com>
2024-11-30 19:03:02 +0530

    more changes

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -2,37 +2,33 @@ pragma solidity ^0.8.0;
 //SPDX-License-Identifier: MIT
 
 // 1. Create a Twitter Contract
-// 2. Create a mapping between user and tweet 
+// 2. Create a mapping between user and tweet
 // 3. Add function to create a tweet and save it in mapping
 //4. Create a function to get Tweet
- 
 
- // create the struct of tweet way now
-
+// create the struct of tweet way now
 
 contract Twitter {
+    struct Tweet {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
+    }
 
+    mapping(address => Tweet[]) public tweets;
 
-struct Tweet {
-    address author;
-    string content;
-    uint256 timestamp;
-    uint256 likes;
-}
+    function createTweet(string memory _tweet) public {
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+        tweets[msg.sender].push(newTweet);
+    }
 
-mapping(address => Tweet[]) public tweets;
-
-function createTweet(string memory _tweet) public {
-    Tweet memory newTweet = Tweet(
-        msg.sender,
-        _tweet,
-        block.timestamp,
-        0
-    );
-    tweets[msg.sender].push(newTweet);
-}
-
-    function getTweets() public view returns (Tweet[] memory){
+    function getTweets() public view returns (Tweet[] memory) {
         return tweets[msg.sender];
     }
 }
